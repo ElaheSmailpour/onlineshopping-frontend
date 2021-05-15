9a46681485d5c7e96772180337dcc1716bbed0fa
Elahesmailpour <eli_es87@yahoo.com>
2021-05-15 22:55:40 +0200

    logoutlogin1

--- src/Pages/Google.js
+++ src/Pages/Google.js
@@ -1,27 +1,29 @@
 import { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { GoogleLogin } from 'react-google-login';
 import { GoogleLogout } from 'react-google-login';
 const Google = () => {
     const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [url, setUrl] = useState("")
 
+    const [url, setUrl] = useState("")
+    //const history=useHistory()
     const responseGoogle = (response) => {
         console.log("response=", response)
         setName(response.profileObj.name)
-        setEmail(response.profileObj.email)
-        setUrl(response.profileObj.imageUrl)
-    }
 
+        setUrl(response.profileObj.imageUrl)
+        // history.push("/")
+    }
+    const history = useHistory()
     const onSuccess = () => {
 
         alert('Logout made successfully ✌');
+        history.push("/")
     }
     return (
         <div className="Google">
             <h1>Login with Google</h1>
             <h2>Welcome:{name}</h2>
-            <h2>Email: {email}</h2>
             <img src={url} alt={name} />
             <GoogleLogin
                 clientId="828234666913-l6tv3hqml3adrbripk1l548g41sc0n1m.apps.googleusercontent.com"
